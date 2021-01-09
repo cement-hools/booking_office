b82257cbe75369aba9c91d156af5bc5376252d07
Максим Секачёв <cement-fan@ya.ru>
2021-01-09 15:35:57 +0300

    UTC

--- booking_office/settings.py
+++ booking_office/settings.py
@@ -116,9 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'en'
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
