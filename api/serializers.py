d9580aa26208393fb90bb00c7f86a685625e7c83
Максим Секачёв <cement-fan@ya.ru>
2021-01-09 13:19:08 +0300

    user fields

--- api/serializers.py
+++ api/serializers.py
@@ -13,7 +13,7 @@ class UserSerializer(ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('username', 'password', 'first_name', 'last_name',)
 
     def create(self, validated_data):
         user = User.objects.create(username=validated_data['username'])
